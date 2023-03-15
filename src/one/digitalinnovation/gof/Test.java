43f32f4840e8406c73bbc2503654c5386183cb38
Fabio-Genari <fabiogenari22@gmail.com>
2023-03-15 17:33:03 -0300

    praticando java puro: facade

--- src/one/digitalinnovation/gof/Test.java
+++ src/one/digitalinnovation/gof/Test.java
@@ -1,5 +1,6 @@
 package one.digitalinnovation.gof;
 
+import one.digitalinnovation.gof.facade.Facade;
 import one.digitalinnovation.gof.singleton.SingletonEager;
 import one.digitalinnovation.gof.singleton.SingletonLazy;
 import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
@@ -39,5 +40,10 @@ public class Test {
         robo.mover();
         robo.setComportamento(defensivo);
         robo.mover();
+
+        //Facade
+
+        Facade facade = new Facade();
+        facade.migrarCliente("Fabio", "12345678");
     }
 }
